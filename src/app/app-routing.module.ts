a062e496aef04b2e83f4ccd2d894d7490312e5a9
Yannick Akakm Ze-ABBAYAHINSPIRED <48994548+ABBAYAHINSPIRED@users.noreply.github.com>
2020-10-31 16:04:51 -0400

    Connected imports
    Redirected the imports connections

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
-import { Component, NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+ 
 import {HomeComponent} from './home/home.component';
-import {ArticlesFavoriteTipsComponent} from './components/articlesFavoriteTips/articles-favorite-tips.component';
-import {ArticlesFeatureProjectComponent} from './components/articlesFeatureProject/articles-feature-project.component';
-import {EventsComponent} from './components/events/upcoming-events.component';
-import {SuggestionsComponent} from './components/suggestions/suggestions.component';
-import { RegisterComponent } from './components/register/register.component';
-import { LoginComponent } from './components/login/login.component';
-import { ProfileComponent } from './components/profile/profile.component';
+import {FavoritesTipsArticlesComponent} from './favorites-tips-articles/favorites-tips-articles.component';
+import {FeaturedProjectsArticlesComponent} from './featured-projects-articles/featured-projects-articles.component';
+import {UpcomingEventsComponent} from './upcoming-events/upcoming-events.component';
+import {SuggestionsComponent} from './suggestions/suggestions.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   {
@@ -16,16 +16,16 @@ const routes: Routes = [
     component: HomeComponent
   },
   {
-    path: 'articlesFavoriteTips',
-    component: ArticlesFavoriteTipsComponent
+    path: 'favoritestipsarticles',
+    component: FavoritesTipsArticlesComponent
   },
   {
-    path: 'articlesFeatureProject',
-    component: ArticlesFeatureProjectComponent
+    path: 'featured-projects-articles',
+    component: FeaturedProjectsArticlesComponent
   },
   {
-  path: 'events',
-  component: EventsComponent
+  path: 'upcoming-events',
+  component: UpcomingEventsComponent
 },
 {
   path:'suggestions',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {NavBarComponent} from './components/nav-bar/nav-bar.component';
+
+
+import {NavBarComponent} from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
-import {ArticlesFeatureProjectComponent} from '../app/components/articlesFeatureProject/articles-feature-project.component';
-import {ArticlesFavoriteTipsComponent} from './components/articlesFavoriteTips/articles-favorite-tips.component';
-import {EventsComponent} from './components/events/upcoming-events.component';
-import {SuggestionsComponent} from './components/suggestions/suggestions.component';
-import { RegisterComponent } from './components/register/register.component';
-import { LoginComponent } from './components/login/login.component';
-import { ProfileComponent } from './components/profile/profile.component';
+import {FavoritesTipsArticlesComponent} from './favorites-tips-articles/favorites-tips-articles.component';
+import {FeaturedProjectsArticlesComponent} from './featured-projects-articles/featured-projects-articles.component';
+import {UpcomingEventsComponent} from './upcoming-events/upcoming-events.component';
+import {SuggestionsComponent} from './suggestions/suggestions.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     HomeComponent,
-    ArticlesFeatureProjectComponent,
-    ArticlesFavoriteTipsComponent,
-    EventsComponent,
+    FavoritesTipsArticlesComponent,
+    FeaturedProjectsArticlesComponent,
+    UpcomingEventsComponent,
     SuggestionsComponent,
     RegisterComponent,
     LoginComponent,
+    FormsModule,
     ProfileComponent
   ],
   imports: [
