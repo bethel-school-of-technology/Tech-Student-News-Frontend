d9295d437a693b75ec819e1eaa181b25be6f23e7
Yannick Akakm Ze-ABBAYAHINSPIRED <48994548+ABBAYAHINSPIRED@users.noreply.github.com>
2020-10-31 23:05:21 -0400

    Upddated:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -27,7 +26,6 @@ import { ProfileComponent } from './profile/profile.component';
     SuggestionsComponent,
     RegisterComponent,
     LoginComponent,
-    FormsModule,
     ProfileComponent
   ],
   imports: [
